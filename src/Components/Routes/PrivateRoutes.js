54c97a9519bca006a9b58e09f901c7ab2799eccc
Forhad Khan <fk.nafi11@gmail.com>
2022-10-19 14:45:46 +0600

    private router bug fix

--- src/Components/Routes/PrivateRoutes.js
+++ src/Components/Routes/PrivateRoutes.js
@@ -1,16 +1,21 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../../Hooks/AuthHook'
-import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoutes = ({ children }) => {
-    const user = useContext(AuthContext)
+    const { user, loading } = useContext(AuthContext)
+    console.log(user);
     const location = useLocation()
+    if (loading) {
+        return <h1>loading...</h1>
+    }
 
-    if (user && user.uid) {
-        return children
+    if (!user) {
+
+        return <Navigate to='/login' state={{ from: location }} replace />
 
     }
-    return <Navigate to='/login' state={{ from: location }} replace />
+    return children
 }
 
 export default PrivateRoutes
--- src/Hooks/AuthHook.js
+++ src/Hooks/AuthHook.js
@@ -7,31 +7,35 @@ export const AuthContext = createContext()
 
 const auth = getAuth(app);
 
-
-
 const AuthHook = ({ children }) => {
     const [user, setUser] = useState(null)
 
+
+    const [loading, setLoading] = useState(true)
+
     // CreateAccount
     const createNewUser = (email, password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
     // login user
     const logInUser = (email, password) => {
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
     // logOut
-   
+
     // hold user state data
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
+            setLoading(false)
         })
         return () => unsubscribe()
     }, [])
 
-    const values = { user, createNewUser, logInUser}
+    const values = { user, createNewUser, logInUser, loading }
     return (
         <AuthContext.Provider value={values} >
             {children}
